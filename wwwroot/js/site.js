9a08e3d877740202e255a60a429b1296ecc7bc07
Ali Mousavi <amousavig@icloud.com>
2022-03-24 03:24:22 +0430

    Added desktop mode.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,23 +1,75 @@
 ﻿// DARK MODE
-if (localStorage.getItem('darkmode') === 'true') {
-    $('html').addClass('dark');
-    $('#dark-mode-dark').prop('checked', true);
-    $('#dark-mode-light').prop('checked', false)
-} else if(localStorage.getItem('darkmode') === 'false') {
-    $('html').removeClass('dark');
-    $('#dark-mode-dark').prop('checked', false);
-    $('#dark-mode-light').prop('checked', true);
-} else {
-    $('#dark-mode-dark').prop('checked', false);
-    $('#dark-mode-light').prop('checked', true);
+// set darkmode cookie
+function setDarkModeCookie(val) {
+    var d = new Date();
+    d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = 'darkmode=' + val + ';' + expires + ';path=/';
 }
-$(document).on('change', '[name="dark-mode"]', function() {
+// get darkmode cookie
+function getDarkModeCookie() {
+    var name = "darkmode=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length) == 'true';
+        }
+    }
+    return "";
+}
+
+if (getDarkModeCookie() == true) {
+    $('.dark-mode').each(function (i, elem) {
+        $(elem).prop('checked', true);
+    });
+    $('.light-mode').each(function(i, elem) {
+        $(elem).prop('checked', false);
+    });
+} else if(getDarkModeCookie() == false) {
+    $('.dark-mode').each(function (i, elem) {
+        $(elem).prop('checked', false);
+    });
+    $('.light-mode').each(function(i, elem) {
+        $(elem).prop('checked', true);
+    });
+} else {
+    $('.dark-mode').each(function (i, elem) {
+        $(elem).prop('checked', false);
+    });
+    $('.light-mode').each(function(i, elem) {
+        $(elem).prop('checked', true);
+    });
+}
+$(document).on('change', '[name="dark-mode"], [name="dark-mode-desktop"]', function() {
     var checkedRadio = $(this);
-    if (checkedRadio.attr('data-mode') === 'dark') {
-        localStorage.setItem('darkmode', true);
+    if (checkedRadio.hasClass('dark-mode')) {
+        setDarkModeCookie(true);
         $('html').addClass('dark');
     } else {
-        localStorage.setItem('darkmode', false);
+        setDarkModeCookie(false);
         $('html').removeClass('dark');
     }
+});
+// MODAL
+$(document).on('click', '[data-modal]', function() {
+    var modalId = $(this).attr('data-modal');
+    $('#' + modalId).addClass('active');
+    $('body').addClass('modal-active');
+});
+$(document).on('click', '.modal-close', function() {
+    $(this).closest('.modal').removeClass('active');
+    $('body').removeClass('modal-active');
+});
+// HIDE MODAL ON CLICK OUTSIDE
+$(document).on('click', '.modal', function(e) {
+    if (e.target !== this) {
+        return;
+    }
+    $(this).removeClass('active');
+    $('body').removeClass('modal-active');
 });
